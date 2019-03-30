02c436d3c69ad14d876d6f1df4e7e1f9a48a0f1f
cwatson45 <cwatson45@users.noreply.github.com>
2019-03-30 10:24:34 -0400

    data parseing code

--- xml_parsing.py
+++ xml_parsing.py
@@ -3,14 +3,33 @@ import xml.etree.cElementTree as ET
 import xml.dom.minidom
 import numpy as np
 
-filename = "data/en/truth-train.txt"
-
-
-table = np.genfromtxt(filename, delimiter = ':::', dtype = 'U20')
-print(table)
-
-def readData()
+def readData(id_list):
     filepath = "data/en/"
-    for id in table[:,0]:
-    f = filepath + id
-    read 
+    #data in the form {'id': 'tweet body'}
+    data_train = {}
+    for id in id_list:
+        xmlfile = filepath + id +".xml"
+        print("Parsing ", xmlfile)
+        tweet_list = parseXML(xmlfile)
+        data_train[id] = tweet_list
+    return data_train
+
+
+def parseXML(xmlfile): 
+    tree = ET.parse(xmlfile) 
+    root = tree.getroot()
+    tweets = root[0]
+    tweet_list = []
+    for t in tweets:
+        cdatatext = t.text
+        tweet_list.append(cdatatext)
+    return tweet_list
+
+
+filename = "data/en/truth-train.txt"
+truth_train = np.genfromtxt(filename, delimiter = ':::', dtype = 'U32')
+data_train = readData(truth_train[:,0])
+print(len(data_train), 'training examples')
+print(len(data_train['754903b310327bbf56600ac33f205a81']), 'tweets in each example')
+
+
